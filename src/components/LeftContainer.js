70ada9adf8d25c6e48d8b1c0cce958ae032700b0
qzmp10 <loganrobitaille@outlook.com>
2022-11-15 20:45:00 -0500

    ui css

--- src/components/LeftContainer.js
+++ src/components/LeftContainer.js
@@ -4,10 +4,12 @@ import { FaRegEnvelope, FaUser } from 'react-icons/fa'
 
 export default function LeftContainer(props) {
 
+
     const explore = useRef();
 
+    //do animations for hover
     const hover = () => {
-        explore.current.style.backgroundColor = 'rgba(255, 145, 145, 0.526)'
+        explore.current.style.backgroundColor = 'rgba(255, 145, 145, 0.326)'
     }
     const outHover = () => {
         explore.current.style.backgroundColor='white';
--- src/components/MiddleContainer.js
+++ src/components/MiddleContainer.js
@@ -1,6 +1,12 @@
 
 export default function MiddleContainer(props) {
-    return(
-        <div className='main container column middle alignCenter'>hello world</div>
+    return (
+        <div className='main container column middle alignCenter'>
+            <div className='middle-bar'>
+                <span>
+                    <input type='text' placeholder="Search" />
+                </span>
+            </div>
+        </div>
     )
 }
