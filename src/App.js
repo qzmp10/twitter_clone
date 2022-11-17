d4856e450a47645b237e428a9d35274b3de6bad1
qzmp10 <loganrobitaille@outlook.com>
2022-11-17 12:13:39 -0500

    state & callback for ui when signed in

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import LeftContainer from './components/LeftContainer';
 import MiddleContainer from './components/MiddleContainer';
 import RightContainer from './components/RightContainer';
+import { useState } from 'react';
 
 function App() {
+
+  const [signedInStatus, setSignedInStatus] = useState(false);
+  
+  const callbackSignedInStatus = (status) => {
+    setSignedInStatus(true);
+  }
+
   return (
     <div className='App'>
-      <LeftContainer/>
-      <MiddleContainer/>
-      <RightContainer/>
+      <LeftContainer signedIn={signedInStatus}/>
+      <MiddleContainer signedIn={signedInStatus}/>
+      <RightContainer signedIn={signedInStatus}/>
     </div>
 
   );
