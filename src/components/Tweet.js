55dce199199054f3c6efe6e83f8686d95add0ecc
qzmp10 <loganrobitaille@outlook.com>
2022-11-20 19:08:27 -0500

    user tweets stored in firebase

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,13 +1,38 @@
 import { FaGlobeAmericas, FaRegImage, FaRegWindowClose, FaSmile } from "react-icons/fa";
 import { AiOutlineGif } from 'react-icons/ai'
 import { useState } from "react";
+import { arrayUnion, doc, updateDoc, serverTimestamp, Timestamp} from "firebase/firestore";
+import { db } from "../firebase.config";
+import {  useEffect } from "react";
 
 export default function Tweet(props) {
 
-    const [tweetContent, setTweetContent] = useState()
+    const [tweetContent, setTweetContent] = useState('');
+
+    useEffect(() => {
+        console.log(props.user)
+    }, [])
+
     const changeTweetStatus = () => {
         props.tweet(false);
     }
+
+    const inputChange = (e) => {
+        setTweetContent(e.target.value);
+        console.log(tweetContent)
+    }
+
+    async function updateTweets() {
+        const userData  = await doc(db, 'users', `${props.user}`);
+
+
+
+        await updateDoc(userData, {
+            tweets: arrayUnion({text: tweetContent, 
+            timestamp: Timestamp.now()}),
+        })
+    }
+
     return(
         <div className='tweet-popUp'>
             <div className="x" onClick={changeTweetStatus}><FaRegWindowClose/></div>
@@ -16,7 +41,7 @@ export default function Tweet(props) {
             </div>
             <div className='tweet-right'>
                 <div className='everyone'>Everyone</div>
-                <textarea placeholder="What's happening?"></textarea>
+                <textarea placeholder="What's happening?" value={tweetContent} onChange={inputChange}></textarea>
                 <div className="reply-options"><FaGlobeAmericas /> Everyone can reply</div>
                 <div className="right-bottom">
                     <div className='bottom-icon'>
@@ -29,7 +54,10 @@ export default function Tweet(props) {
                         <FaSmile />
                     </div>
                     <div className='bottom-chat'>
-                        <div className='bottom-chat-button'>
+                        <div className='bottom-chat-button' onClick={() => {
+                            updateTweets();
+                            changeTweetStatus();
+                        }}>
                             Chat
                         </div>
                     </div>
